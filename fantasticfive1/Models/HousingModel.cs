8c25d4590b1b4dba5a598e0e6af6664ddc380788
Angelo Milonas <angelomilonas1@gmail.com>
2024-09-21 01:49:36 -0400

    Home changes

---
+++ fantasticfive1/Models/HousingModel.cs
@@ -0,0 +1,17 @@
+﻿namespace fantasticfive1.Models
+{
+    public class HousingModel
+    {
+        // String properties
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ShelterType { get; set; }
+        public string PhoneNumber { get; set; }
+        public string hours { get; set; }
+
+        // Boolean properties
+        public bool Womens { get; set; }
+        public bool ChildFriendly { get; set; }
+        public bool AllWelcome { get; set; }
+    }
+}
